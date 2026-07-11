148c18a8b048a17f950962819485c0202376db3c
Vick Javariz <66493562+vickjavariz@users.noreply.github.com>
2026-07-11 01:28:13 -0400

    build: add and integrate prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -18,4 +19,5 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+  eslintConfigPrettier
 ])
