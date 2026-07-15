c6457718e10f919998161405d6a13b28b8b9e39f
Vick Javariz <66493562+vickjavariz@users.noreply.github.com>
2026-07-14 23:38:29 -0400

    build: fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,28 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { reactRefresh } from 'eslint-plugin-react-refresh';
+
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
   globalIgnores(['dist', '**/routeTree.gen.ts']),
+
+  js.configs.recommended,
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite({
+    extraHOCs: ['createRootRoute', 'createFileRoute'],
+  }),
+
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite({
-        extraHOCs: ['createRootRoute', 'createFileRoute'],
-      }),
-    ],
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
+
   eslintConfigPrettier,
 ]);
