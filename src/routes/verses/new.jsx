32f9fdd54f3abc29055bf412159c62304874be03
Vick Javariz <66493562+vickjavariz@users.noreply.github.com>
2026-07-17 01:38:32 -0400

    feat: add back button to /verses/new

--- src/routes/verses/new.jsx
+++ src/routes/verses/new.jsx
@@ -1,17 +1,26 @@
-import { Link, createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+
+import { ArrowLeftIcon } from "@/components/icons";
 
 export const Route = createFileRoute("/verses/new")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const router = useRouter();
+
   return (
     <>
-      <h1 className="text-xl font-bold">New Verse</h1>
-
-      <Link to="/" className="mt-5">
-        Cancel
-      </Link>
+      <div className="flex items-center font-bold">
+        <button
+          className="btn btn-circle btn-ghost -m-2 hover:bg-neutral"
+          type="button"
+          onClick={() => router.history.back()}
+        >
+          <ArrowLeftIcon className="text-3xl" />
+        </button>
+        <h1 className="text-xl ml-4">New Verse</h1>
+      </div>
     </>
   );
 }
