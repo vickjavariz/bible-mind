d01433aadebc2b310a6ad1ee05aea8900830e955
Vick Javariz <66493562+vickjavariz@users.noreply.github.com>
2026-07-20 00:45:07 -0400

    build: configure eslint for typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,26 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
-import globals from 'globals';
-
-import js from '@eslint/js';
-import reactHooks from 'eslint-plugin-react-hooks';
-import { reactRefresh } from 'eslint-plugin-react-refresh';
-
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import reactHooks from "eslint-plugin-react-hooks";
+import { reactRefresh } from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(['dist', '**/routeTree.gen.ts']),
-
-  js.configs.recommended,
-  reactHooks.configs.flat.recommended,
-  reactRefresh.configs.vite({
-    extraHOCs: ['createRootRoute', 'createFileRoute'],
-  }),
-
+  globalIgnores(["dist", "**/routeTree.gen.ts"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite({
+        extraHOCs: ["createRootRoute", "createFileRoute"],
+      }),
+    ],
     languageOptions: {
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
-
   eslintConfigPrettier,
 ]);
