980190bb61124d4de300181eebc43ab652df512a
Vick Javariz <66493562+vickjavariz@users.noreply.github.com>
2026-07-20 00:50:13 -0400

    build: configure tanstack router for typescript

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,23 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 import { routeTree } from "@/routeTree.gen";
 
-import "./i18n/i18n";
+import "./i18n/i18n.ts";
 import "./index.css";
 
 const router = createRouter({ routeTree });
 
-createRoot(document.getElementById("root")).render(
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
     <TanStackRouterDevtools
       router={router}
+      // @ts-expect-error - Devtools expects class rather than className
       toggleButtonProps={{ class: "!hidden md:!flex" }}
     />
   </StrictMode>,
