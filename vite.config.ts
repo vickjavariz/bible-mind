abf5470f2d4c06b02f8259d49d0ca6fcfd7cbbca
Vick Javariz <66493562+vickjavariz@users.noreply.github.com>
2026-07-19 22:02:12 -0400

    build: add and set up typescript

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
