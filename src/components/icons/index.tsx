670b51f3895e0064687ba872d43df97821aa4b87
Vick Javariz <66493562+vickjavariz@users.noreply.github.com>
2026-07-21 00:15:27 -0400

    chore: add cog icon asset and component

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,4 +1,5 @@
 export { default as ArrowLeftIcon } from "./ArrowLeftIcon";
 export { default as BibleIcon } from "./BibleIcon";
 export { default as BookOpenIcon } from "./BookOpenIcon";
+export { default as CogIcon } from "./CogIcon";
 export { default as PlusIcon } from "./PlusIcon";
