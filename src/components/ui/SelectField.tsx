daec77a0de5032002a6af0c6bc6b316e512fca9d
Vick Javariz <66493562+vickjavariz@users.noreply.github.com>
2026-07-30 19:57:24 -0400

    refactor: add useId to select field component

--- src/components/ui/SelectField.tsx
+++ src/components/ui/SelectField.tsx
@@ -1,4 +1,4 @@
-import type { ChangeEvent, ReactNode } from "react";
+import { type ChangeEvent, type ReactNode, useId } from "react";
 
 interface SelectFieldProps {
   label: string;
@@ -17,12 +17,14 @@ export default function SelectField({
   onChange,
   className,
 }: SelectFieldProps) {
+  const id = useId();
   const mergedClassName = `label flex flex-col items-start ${className || ""}`;
 
   return (
     <label className={mergedClassName}>
       <span className="text-sm font-bold">{label.toUpperCase()}</span>
       <select
+        id={id}
         className="select w-full bg-base-300 shadow-sm"
         value={value}
         onChange={onChange}
